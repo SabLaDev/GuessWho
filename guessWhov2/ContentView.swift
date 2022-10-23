962e22245d8e628d886668e7c86fa689d1e63bd5
Sabrina AUGUSTY <sabrina.augusty@gmail.com>
2022-10-23 15:13:34 +0200

    Adding image as a background who change with the logic and the button who launch the display of a random guest

--- guessWhov2/ContentView.swift
+++ guessWhov2/ContentView.swift
@@ -8,14 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isKnocked = "🚪TOC! TOC! TOC!🚪"
+    private var guests = ["👻 Casper","🧛‍♂️ Dracula","🎅🏻 Le Père Noël","🕵️‍♂️ Sherlock HOLMES","👨‍🔬 Bruce BANNER","👨🏼‍🎨 Léonard DE VINCI", "👨🏻‍💻 Mark ZUCKERBERG"]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            if(isKnocked == "🚪TOC! TOC! TOC!🚪"){
+                Image("whoIsAtTheDoor")
+            } else {
+                Image("welcome")
+            }
+           
+            VStack {
+                Text("❓Devine qui vient dîner❓")
+                    .font(.largeTitle)
+                    .padding()
+                        
+                Text(isKnocked)
+                    .font(.title)
+                    .padding()
+                    .bold()
+                
+                Spacer()
+                
+                Button {
+                    if(isKnocked == "🚪TOC! TOC! TOC!🚪"){
+                        isKnocked = guests.randomElement()!
+                    }else{
+                        isKnocked = "🚪TOC! TOC! TOC!🚪"
+                    }
+                } label: {
+                    Text("Qui est là?")
+                        .font(.title)
+                }
+                .padding()
+                .background(.green)
+                .foregroundColor(.white)
+                .cornerRadius(15)
+                
+                Spacer()
+                
+                Text("Made by Sabrina AUGUSTY")
+                    .font(.footnote)
+                    .foregroundColor(.cyan)
+                
+            }
         }
-        .padding()
+        
     }
 }
 
