cf94e4a56e6c8d318d47ce735246cf69c58ced62
Sabrina AUGUSTY <sabrina.augusty@gmail.com>
2022-10-23 14:47:32 +0200

    first commit

---
+++ guessWhov2/guessWhov2App.swift
@@ -0,0 +1,17 @@
+//
+//  guessWhov2App.swift
+//  guessWhov2
+//
+//  Created by Apprenant 77 on 23/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct guessWhov2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
